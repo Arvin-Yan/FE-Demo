c09dcf377d0f1286f9621cb01f03585997175895
ykw <675503578@qq.com>
2016-08-08 19:36:21 +0800

    edit gotop

--- carousel-Ajax/js/com/goTop.js
+++ carousel-Ajax/js/com/goTop.js
@@ -2,16 +2,12 @@ define(['jquery'], function($){
     var goTop = (function() {
             var $goTop = $('<div id="goTop">回到顶部</div>'),
                 $body = $('body');
-            $goTop.data('unloaded', true);
 
             function init() {
-                if ($goTop.data('unloaded')) {
-                    $body.append($goTop);
-                    $goTop.data('unloaded', false);
-                }
+                $body.append($goTop);
                 $(window).on('scroll', function() {
                     var offsetTop = $body.scrollTop();
-                    if (offsetTop > 200) {
+                    if (offsetTop > 300) {
                         $goTop.show();
                     } else {
                         $goTop.hide();
